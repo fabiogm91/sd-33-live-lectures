4b06605c2bae9831a6f2fe8e535f8f91136ecbe8
fabiogm91 <machadofabio91@gmail.com>
2023-05-03 17:12:28 -0300

    fazendo jogo da loteria

---
+++ script.js
@@ -0,0 +1,11 @@
+let num1 = 5;
+let num2 = 10;
+// soma
+let soma = num1 + num2;
+console.log(soma);
+
+let sub = num1 - num2;
+console.log(sub);
+
+let mult = num1 * num2;
+console.log(mult);
---
+++ scripts-teste.js
@@ -0,0 +1,19 @@
+let jogoDoFabio = [22, 4, 37, 39, 55, 42];
+
+//console.log(jogoDoFabio);
+//console.log(jogoDoFabio [0]);
+//console.log(jogoDoFabio [1]);
+//console.log(jogoDoFabio [2]);
+//console.log(jogoDoFabio [3]);
+//console.log(jogoDoFabio [4]);
+//console.log(jogoDoFabio [5]);
+
+//console.log(jogoDoFabio.length);
+
+//for (let index = 0; index <6; index += 1) {
+    console.log(jogoDoFabio [index]);
+//}
+
+//for (let values of jogoDoFabio) {
+    console.log(values);
+//}
